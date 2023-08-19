39e72090b543791a54a8c04bf1678eb023ee30fb
samisagit <sam@whiteteam.co.uk>
2023-08-19 13:23:00 +0100

    Add root flake for maintaining the lab

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A flake to develop against my homelab";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+	defaultPackage = pkgs.hello;
+
+        devShell = pkgs.mkShell{ 
+          buildInputs = [
+            pkgs.gnumake
+          ];
+        };
+      });
+}
